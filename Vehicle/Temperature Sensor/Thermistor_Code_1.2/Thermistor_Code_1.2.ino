29a676bb05dd0595fc70849fe78b0495b66e6642
19etweinstock <19etweinstock@users.noreply.github.com>
2019-10-01 19:09:43 -0400

    Verified code works

--- Vehicle/Temperature Sensor/Thermistor_Code_1.2/Thermistor_Code_1.2.ino
+++ Vehicle/Temperature Sensor/Thermistor_Code_1.2/Thermistor_Code_1.2.ino
@@ -1,4 +1,4 @@
-#define in_pin A0
+#define in_pin A3
  
 void setup() {
   // put your setup code here, to run once:
@@ -16,8 +16,8 @@ void loop() {
   Serial.println(r2);
   double degKCalc = (3950 * 298.15)/(298.15*log(r2/10000)+3950); 
   double degCCalc = degKCalc - 273.15;
-  double adjustment = -0.0000000000069136 * pow(degCCalc,6) + 0.0000000018528289 * pow(degCCalc,5)+ -0.0000001759253389 * pow(degCCalc,4) 
-    + 0.0000127535398116 * pow(degCCalc,3) + -0.0014376940338028 * pow(degCCalc,2) +0.0763055743553628 *degCCalc + -1.1574790782205100;
+  double adjustment = -0.0000000000069136 * pow(degCCalc,6) + 0.0000000018528289 * pow(degCCalc,5) -0.0000001759253389 * pow(degCCalc,4) 
+    + 0.0000127535398116 * pow(degCCalc,3) -0.0014376940338028 * pow(degCCalc,2) +0.0763055743553628 *degCCalc -1.1574790782205100;
   double degC = degCCalc + adjustment;
   Serial.println(degC);
   Serial.println();
