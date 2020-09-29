594c7aebc65501f93d30815dc2d4b6db919700fe
atulmerchia <atulmerchia@gmail.com>
2020-09-29 01:24:38 -0700

    added some library files

--- Testing/libraries/Serial/Serial.h
+++ Testing/libraries/Serial/Serial.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <fstream>
+#include <sstream>
+#include <bitset>
 
 class MockSerial {
 public:
@@ -7,28 +9,25 @@ public:
     ~MockSerial();
     void init(std::string filepath);
     void begin(unsigned int baudRate);
-    void print(bool);               void println(bool);
-    void print(char);               void println(char);
-    void print(unsigned char);      void println(unsigned char);
-    void print(short);              void println(short);
-    void print(unsigned short);     void println(unsigned short);
-    void print(int);                void println(int);
-    void print(unsigned int);       void println(unsigned int);
-    void print(long);               void println(long);
-    void print(unsigned long);      void println(unsigned long);
-    void print(long long);          void println(long long);
-    void print(unsigned long long); void println(unsigned long long);
-    void print(float);              void println(float);
-    void print(double);             void println(double);
-    void print(long double);        void println(long double);
-    void print(std::string);        void println(std::string);
-    void print(const char*);        void println(const char*);
-    template <typename T> inline void print(T value);
-    template <typename T> inline void println(T value);
+    template <typename T> inline void print(T value) { validate(); file << value; }
+    template <typename T> inline void print(T value, int base) { validate(); file << format(value, base).rdbuf(); }
+    inline void println() { validate(); file << '\n'; }
+    template <typename T> inline void println(T value) { validate(); file << value << '\n'; }
+    template <typename T> inline void println(T value, int base) { validate(); file << format(value, base).rdbuf() << '\n'; }
 private:
     int fId;
     std::string fFilepath;
     std::ofstream file;
+    void validate();
+
+    template <typename T> inline std::stringstream format(T value, int base) {
+        std::stringstream ss;
+        if (base == 2) ss << std::bitset<sizeof(value) << 3>(value);
+        else if (base == 8) ss << std::oct << value;
+        else if (base == 16) ss << std::hex << value;
+        else ss << value;
+        return ss;
+    }
 };
 
 extern MockSerial Serial, Serial2;
