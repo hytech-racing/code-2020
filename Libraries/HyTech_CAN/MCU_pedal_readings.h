b0e2f1f881b71e9e7b5c192196293d5e64552ccf
19etweinstock <19etweinstock@users.noreply.github.com>
2020-11-15 18:47:30 -0500

    Can cleanup

--- Libraries/HyTech_CAN/MCU_pedal_readings.h
+++ Libraries/HyTech_CAN/MCU_pedal_readings.h
@@ -1,52 +1,48 @@
-#ifndef __MCU_PEDAL_READINGS_H__
-#define __MCU_PEDAL_READINGS_H__
-
+#pragma once
+#include <string.h>
 #include <cstdint>
-#include <string>
 
 #pragma pack(push,1)
-
-typedef struct CAN_message_mcu_pedal_readings_t {
-    uint16_t accelerator_pedal_raw_1;
-    uint16_t accelerator_pedal_raw_2;
-    uint16_t brake_transducer1;
-    uint8_t pedal_flags;
-    uint8_t torque_map_mode;
-} CAN_message_mcu_pedal_readings_t;
 
 class MCU_pedal_readings {
 public:
     MCU_pedal_readings() = default;
+    MCU_pedal_readings(uint8_t buf[8]) { load(buf); }
+    MCU_pedal_readings(uint16_t accelerator_pedal_raw_1, uint16_t accelerator_pedal_raw_2, uint16_t brake_pedal_raw, uint8_t pedal_flags, uint8_t torque_map_mode) {
+        set_accelerator_pedal_raw_1(accelerator_pedal_raw_1);
+        set_accelerator_pedal_raw_2(accelerator_pedal_raw_2);
+        set_brake_pedal_raw(brake_pedal_raw);
+        set_pedal_flags(pedal_flags);
+        set_torque_map_mode(torque_map_mode);
+    }
 
-    MCU_pedal_readings(const uint8_t buf[8]) { load(buf); }
+    inline void load(uint8_t buf[])     { memcpy(this, buf, sizeof(*this)); }
+    inline void write(uint8_t buf[])    { memcpy(buf, this, sizeof(*this)); }
 
-    inline void load(const uint8_t buf[8]) { memcpy(this, buf, sizeof(*this)); }
-    inline void write(uint8_t buf[8]) const { memccpy(buf, this, sizeof(*this)); }
+    inline uint16_t get_accelerator_pedal_raw_1()   const { return accelerator_pedal_raw_1; }
+    inline uint16_t get_accelerator_pedal_raw_2()   const { return accelerator_pedal_raw_2; }
+    inline uint16_t get_brake_pedal_raw()           const { return brake_pedal_raw; }
+    inline uint8_t  get_pedal_flags()               const { return pedal_flags; }
 
-    void write(uint8_t buf[8]);
-    uint16_t get_accelerator_pedal_raw_1();
-    uint16_t get_accelerator_pedal_raw_2();
-    uint16_t get_brake_pedal_raw();
-    uint8_t get_pedal_flags();
-    bool get_accelerator_implausibility();
-    bool get_brake_implausibility();
-    bool get_brake_pedal_active();
-    uint8_t get_torque_map_mode();
-    void set_accelerator_pedal_raw_1(uint16_t accelerator_pedal_raw_1);
-    void set_accelerator_pedal_raw_2(uint16_t accelerator_pedal_raw_2);
-    void set_brake_pedal_raw(uint16_t brake_pedal_raw);
-    void set_pedal_flags(uint8_t pedal_flags);
-    void set_accelerator_implausibility(bool accelerator_implausibility);
-    void set_brake_implausibility(bool brake_implausibility);
-    void set_brake_pedal_active(bool brake_pedal_active);
-    void set_torque_map_mode(uint8_t torque_map_mode);
+    inline bool get_accelerator_implausibility()    const { return pedal_flags & 0x01; }
+    inline bool get_brake_implausibility()          const { return pedal_flags & 0x02; }
+    inline bool get_brake_pedal_active()            const { return pedal_flags & 0x04; }
+    inline uint8_t get_torque_map_mode()            const { return torque_map_mode; }
+
+    inline void set_accelerator_pedal_raw_1(uint16_t accelerator_pedal_raw_1)   { this->accelerator_pedal_raw_1 = accelerator_pedal_raw_1; }
+    inline void set_accelerator_pedal_raw_2(uint16_t accelerator_pedal_raw_2)   { this->accelerator_pedal_raw_2 = accelerator_pedal_raw_2; }
+    inline void set_brake_pedal_raw(uint16_t brake_pedal_raw)                   { this->brake_pedal_raw = brake_pedal_raw; }
+    inline void set_pedal_flags(uint8_t pedal_flags)                            { this->pedal_flags = pedal_flags; }
+    inline void set_accelerator_implausibility(bool accelerator_implausibility) { pedal_flags = (pedal_flags & 0xFE) | accelerator_implausibility; }
+    inline void set_brake_implausibility(bool brake_implausibility)             { pedal_flags = (pedal_flags & 0xFD) | (brake_implausibility << 1); }
+    inline void set_brake_pedal_active(bool brake_pedal_active)                 { pedal_flags = (pedal_flags & 0xFB) | (brake_pedal_active << 2); }
+    inline void set_torque_map_mode(uint8_t torque_map_mode)                    { this->torque_map_mode = torque_map_mode; }
 private:
     uint16_t accelerator_pedal_raw_1;
     uint16_t accelerator_pedal_raw_2;
-    uint16_t brake_transducer1;
-    uint16_t brake_transducer2;
+    uint16_t brake_pedal_raw;
+    uint8_t pedal_flags;
+    uint8_t torque_map_mode;
 };
 
 #pragma pack(pop)
-
-#endif
--- Libraries/HyTech_CAN/MCU_status.h
+++ Libraries/HyTech_CAN/MCU_status.h
@@ -1,90 +1,53 @@
-#ifndef __MCU_STATUS_H__
-#define __MCU_STATUS_H__
-
+#pragma once
+#include <string.h>
 #include <cstdint>
-#include <string>
-
-#define MCU_STATE_TRACTIVE_SYSTEM_NOT_ACTIVE 1
-#define MCU_STATE_TRACTIVE_SYSTEM_ACTIVE 2
-#define MCU_STATE_ENABLING_INVERTER 3
-#define MCU_STATE_WAITING_READY_TO_DRIVE_SOUND 4
-#define MCU_STATE_READY_TO_DRIVE 5
 
 #pragma pack(push,1)
 
-/* lost: need to add
- *  bool MCU_pedal_readings::get_accelerator_implausibility() {
- *  bool MCU_pedal_readings::get_brake_implausibility() {
- *  bool MCU_pedal_readings::get_brake_pedal_active() {
- *  uint8_t MCU_pedal_readings::get_torque_map_mode() {
- */
-
-typedef struct CAN_message_mcu_status_t {
-    /*
-     * OKHS
-     * BMS_OK
-     * BSPD
-     * software shutdown
-     */
-    uint8_t shutdown_inputs;
-    /*
-     * shutdown A
-     * shutdown B
-     * shutdown C
-     * shutdown D
-     * shutdown E
-     * Current high
-     * brake high
-     */
-    uint8_t shutdown_states;
-    int16_t temperature;
-    uint16_t glv_battery_voltage;
-} CAN_message_mcu_status_t;
-
-#pragma pack(pop)
-
 class MCU_status {
 public:
-    MCU_status() { message = {}; }
-    
-    MCU_status(uint8_t buf[8]){
-        message = {};
-
-        memcpy(&(message.state), &buf[0], sizeof(uint8_t));
-        memcpy(&(message.flags), &buf[1], sizeof(uint8_t));
-        memcpy(&(message.temperature), &buf[2], sizeof(int16_t));
-        memcpy(&(message.glv_battery_voltage), &buf[4], sizeof(uint16_t));
+    MCU_status() = default;
+    MCU_status(uint8_t buf[8]) { load(buf); }
+    MCU_status(uint8_t state, uint8_t flags, int16_t temperature, uint16_t glv_battery_voltage) {
+        set_state(state);
+        set_flags(flags);
+        set_temperature(temperature);
+        set_glv_battery_voltage(glv_battery_voltage);
     }
 
-    MCU_status(uint8_t state, uint8_t flags, int16_t temperature, uint16_t glv_battery_voltage);
-    void load(uint8_t buf[8]);
-    void write(uint8_t buf[8]);
-    uint8_t get_state();
-    uint8_t get_flags();
-    bool get_bms_ok_high();
-    bool get_imd_okhs_high();
-    bool get_inverter_powered();
-    bool get_shutdown_b_above_threshold();
-    bool get_shutdown_c_above_threshold();
-    bool get_shutdown_d_above_threshold();
-    bool get_shutdown_e_above_threshold();
-    bool get_shutdown_f_above_threshold();
-    int16_t get_temperature();
-    uint16_t get_glv_battery_voltage();
-    void set_state(uint8_t state);
-    void set_flags(uint8_t flags);
-    void set_bms_ok_high(bool bms_ok_high);
-    void set_imd_okhs_high(bool imd_okhs_high);
-    void set_inverter_powered(bool inverter_powered);
-    void set_shutdown_b_above_threshold(bool shutdown_b_above_threshold);
-    void set_shutdown_c_above_threshold(bool shutdown_c_above_threshold);
-    void set_shutdown_d_above_threshold(bool shutdown_d_above_threshold);
-    void set_shutdown_e_above_threshold(bool shutdown_e_above_threshold);
-    void set_shutdown_f_above_threshold(bool shutdown_f_above_threshold);
-    void set_temperature(int16_t temperature);
-    void set_glv_battery_voltage(uint16_t glv_battery_voltage);
+    inline void load(uint8_t buf[])     { memcpy(this, buf, sizeof(*this)); }
+    inline void write(uint8_t buf[])    { memcpy(buf, this, sizeof(*this)); }
+
+    inline uint8_t get_state()                      const { return state; }
+    inline uint8_t get_flags()                      const { return flags; }
+    inline bool get_bms_ok_high()                   const { return flags & 0x01; }
+    inline bool get_imd_okhs_high()                 const { return flags & 0x02; }
+    inline bool get_inverter_powered()              const { return flags & 0x04; }
+    inline bool get_shutdown_b_above_threshold()    const { return flags & 0x08; }
+    inline bool get_shutdown_c_above_threshold()    const { return flags & 0x10; }
+    inline bool get_shutdown_d_above_threshold()    const { return flags & 0x20; }
+    inline bool get_shutdown_e_above_threshold()    const { return flags & 0x40; }
+    inline bool get_shutdown_f_above_threshold()    const { return flags & 0x80; }
+    inline int16_t get_temperature()                const { return temperature; }
+    inline uint16_t get_glv_battery_voltage()       const { return glv_battery_voltage; }
+
+    inline void set_state(uint8_t state)                                        { this->state = state; }
+    inline void set_flags(uint8_t flags)                                        { this->flags = flags; }
+    inline void set_bms_ok_high(bool bms_ok_high)                               { flags = (flags & 0xFE) | bms_ok_high; }
+    inline void set_imd_okhs_high(bool imd_okhs_high)                           { flags = (flags & 0xFD) | (imd_okhs_high << 1); }
+    inline void set_inverter_powered(bool inverter_powered)                     { flags = (flags & 0xFB) | (inverter_powered << 2); }
+    inline void set_shutdown_b_above_threshold(bool shutdown_b_above_threshold) { flags = (flags & 0xF7) | (shutdown_b_above_threshold << 3); }
+    inline void set_shutdown_c_above_threshold(bool shutdown_c_above_threshold) { flags = (flags & 0xEF) | (shutdown_c_above_threshold << 4); }
+    inline void set_shutdown_d_above_threshold(bool shutdown_d_above_threshold) { flags = (flags & 0xDF) | (shutdown_d_above_threshold << 5); }
+    inline void set_shutdown_e_above_threshold(bool shutdown_e_above_threshold) { flags = (flags & 0xBF) | (shutdown_e_above_threshold << 6); }
+    inline void set_shutdown_f_above_threshold(bool shutdown_f_above_threshold) { flags = (flags & 0x7F) | (shutdown_f_above_threshold << 7); }
+    inline void set_temperature(int16_t temperature)                            { this->temperature = temperature; }
+    inline void set_glv_battery_voltage(uint16_t glv_battery_voltage)           { this->glv_battery_voltage = glv_battery_voltage; }
 private:
-    CAN_message_mcu_status_t message;
+    uint8_t state;
+    uint8_t flags;
+    int16_t temperature;
+    uint16_t glv_battery_voltage;
 };
 
-#endif
+#pragma pack(pop)
