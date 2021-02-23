fcff62f544a77440130d1b91e9c66ad92a693848
atulmerchia <atulmerchia@gmail.com>
2021-02-22 18:25:20 -0800

    better test

--- Utilities/flexcan_tester_rev2/flexcan_tester_rev2.ino
+++ Utilities/flexcan_tester_rev2/flexcan_tester_rev2.ino
@@ -46,4 +46,26 @@ void loop() {
 	Serial.println(mpr.get_brake_pedal_raw());
 	Serial.println(mpr.get_pedal_flags());
 	Serial.println(mpr.get_torque_map_mode());
+
+	MCU_status ms = handle_message(ID_MCU_STATUS, MCU_status(10,20,30,40));
+	Serial.println(ms.get_bms_ok_high());
+	Serial.println(ms.get_flags());
+	Serial.println(ms.get_glv_battery_voltage());
+	Serial.println(ms.get_inverter_powered());
+	Serial.println(ms.get_shutdown_b_above_threshold());
+	Serial.println(ms.get_shutdown_c_above_threshold());
+	Serial.println(ms.get_shutdown_d_above_threshold());
+	Serial.println(ms.get_shutdown_e_above_threshold());
+	Serial.println(ms.get_shutdown_f_above_threshold());
+	Serial.println(ms.get_shutdown_f_above_threshold());
+	Serial.println(ms.get_state());
+	Serial.println(ms.get_temperature());
+
+	BMS_coulomb_counts bcc = handle_message(ID_BMS_COULOMB_COUNTS, BMS_coulomb_counts(123456, 987654));
+	Serial.println(bcc.get_total_charge());
+	Serial.println(bcc.get_total_discharge());
+
+	BMS_balancing_status bbs = handle_message(ID_BMS_BALANCING_STATUS, BMS_balancing_status(5, 0XDEADBEEF));
+	Serial.println(bbs.get_group_id());
+	Serial.println(bbs.get_balancing());
 }
