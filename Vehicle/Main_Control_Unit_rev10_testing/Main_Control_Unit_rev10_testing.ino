1a62dafb25ab38f46d38bd2fafe8254054632714
19etweinstock <ethangweinstock@gmail.com>
2021-03-31 21:27:23 -0400

    bms diag now in test code

--- Vehicle/Main_Control_Unit_rev10_testing/Main_Control_Unit_rev10_testing.ino
+++ Vehicle/Main_Control_Unit_rev10_testing/Main_Control_Unit_rev10_testing.ino
@@ -10,6 +10,7 @@
 
 // set to true or false for debugging
 #define DEBUG false
+#define BMS_DEBUG_ENABLE true
 
 // Outbound CAN messages
 MCU_pedal_readings mcu_pedal_readings{};
@@ -53,6 +54,24 @@ Metro timer_bms_heartbeat = Metro(0, 1);
 // add dashboard heartbeat
 Metro timer_dashboard_heartbeat = Metro(0, 1);
 Metro timer_software_enable_interval = Metro(TIMER_SOFTWARE_ENABLE, 1);
+
+#if BMS_DEBUG_ENABLE
+
+#define TOTAL_IC 8                      // Number of ICs in the system
+#define CELLS_PER_IC 9                  // Number of cells per IC
+#define THERMISTORS_PER_IC 3            // Number of cell thermistors per IC
+#define PCB_THERM_PER_IC 2              // Number of PCB thermistors per IC
+
+BMS_detailed_voltages bms_detailed_voltages[8][3];
+BMS_detailed_temperatures bms_detailed_temperatures[8];
+BMS_onboard_detailed_temperatures bms_onboard_detailed_temperatures[TOTAL_IC];
+BMS_onboard_temperatures bms_onboard_temperatures;
+BMS_balancing_status bms_balancing_status[(TOTAL_IC + 3) / 4]; // Round up TOTAL_IC / 4 since data from 4 ICs can fit in a single message
+
+Metro timer_bms_print(1000);
+
+#endif
+
 
 /*
  * Variables to store filtered values from ADC channels
@@ -209,6 +228,16 @@ void loop() {
     state_machine();
 
     software_shutdown();
+
+    #if BMS_DEBUG_ENABLE
+    static bool bms_print = false;
+    if(Serial.available()){
+        String a = Serial.readString();
+        if (a == "on") bms_print = true;
+        else if (a == "off") bms_print = false;
+    }
+    if (bms_print && timer_bms_print.check()) print_bms();
+    #endif
 }
 
 inline void state_machine() {
@@ -548,6 +577,31 @@ void parse_can_message() {
                 // eliminate all action buttons to not process twice
                 dashboard_status.set_button_flags(0);
                 break;
+            #if BMS_DEBUG_ENABLE
+            case ID_BMS_DETAILED_TEMPERATURES: {
+                BMS_detailed_temperatures temp_det_temp = BMS_detailed_temperatures(rx_msg.buf);
+                bms_detailed_temperatures[temp_det_temp.get_ic_id()].load(rx_msg.buf);
+                break;
+            }
+            case ID_BMS_DETAILED_VOLTAGES: {
+                BMS_detailed_voltages temp_det_volt = BMS_detailed_voltages(rx_msg.buf);
+                bms_detailed_voltages[temp_det_volt.get_ic_id()][temp_det_volt.get_group_id()].load(rx_msg.buf);
+                break;
+            }
+            case ID_BMS_ONBOARD_TEMPERATURES:
+                bms_onboard_temperatures.load(rx_msg.buf);
+                break;
+            case ID_BMS_ONBOARD_DETAILED_TEMPERATURES: {
+                BMS_onboard_detailed_temperatures temp_onboard_det_temp = BMS_onboard_detailed_temperatures(rx_msg.buf);
+                bms_onboard_detailed_temperatures[temp_onboard_det_temp.get_ic_id()].load(rx_msg.buf);
+                break;
+            }
+            case ID_BMS_BALANCING_STATUS: {
+                BMS_balancing_status temp = BMS_balancing_status(rx_msg.buf);
+                bms_balancing_status[temp.get_group_id()].load(rx_msg.buf);
+                break;
+            }
+            #endif
         }
     }
 }
@@ -849,3 +903,95 @@ inline void update_distance_traveled() {
     // scaled by 100 for digitizing
     mcu_status.set_distance_travelled(((total_ticks_front_left + total_ticks_front_right) / (2.0 * NUM_TEETH)) * WHEEL_CIRCUMFERENCE * 100);
 }
+
+
+#if BMS_DEBUG_ENABLE
+inline void print_bms(){
+    print_cells();
+    print_temps();
+    Serial.print("BMS state: ");
+    Serial.println(bms_status.get_state());
+}
+
+void print_cells() {
+    Serial.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    Serial.println("\t\t\t\tRaw Cell Voltages\t\t\t\t\t\t\tCell Status (Ignoring or Balancing)");
+    Serial.println("\tC0\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8\t\tC0\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8");
+    for (int ic = 0; ic < TOTAL_IC; ic++) {
+        Serial.print("IC"); Serial.print(ic); Serial.print("\t");
+        for (int cell = 0; cell < CELLS_PER_IC; cell++) {
+            double voltage = bms_detailed_voltages[ic][cell / 3].get_voltage(cell % 3) * 0.0001;
+            Serial.print(voltage, 4); Serial.print("V\t");
+        }
+        Serial.print("\t");
+        for (int cell = 0; cell < CELLS_PER_IC; cell++) {
+            int balancing = bms_balancing_status[ic / 4].get_cell_balancing(ic % 4, cell);
+            if (balancing) {
+                Serial.print("BAL");
+            }
+            Serial.print("\t");
+        }
+        Serial.println();
+    }
+    Serial.println();
+    Serial.println("\t\t\t\tDelta from Min Cell");
+    Serial.println("\tC0\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8");
+    for (int ic = 0; ic < TOTAL_IC; ic++) {
+        Serial.print("IC"); Serial.print(ic); Serial.print("\t");
+        for (int cell = 0; cell < CELLS_PER_IC; cell++) {
+            double voltage = (bms_detailed_voltages[ic][cell / 3].get_voltage(cell % 3)-bms_voltages.get_low()) * 0.0001;
+            Serial.print(voltage, 4);
+            Serial.print("V");
+            Serial.print("\t");
+        }
+        Serial.println();
+    }
+    Serial.println();
+    Serial.print("Cell voltage statistics\t\tTotal: "); Serial.print(bms_voltages.get_total() / (double) 1e2, 4); Serial.print("V\t\t");
+    Serial.print("Average: "); Serial.print(bms_voltages.get_average() / (double) 1e4, 4); Serial.print("V\t\t");
+    Serial.print("Min: "); Serial.print(bms_voltages.get_low() / (double) 1e4, 4); Serial.print("V\t\t");
+    Serial.print("Max: "); Serial.print(bms_voltages.get_high() / (double) 1e4, 4); Serial.println("V");
+}
+
+void print_temps() {
+    Serial.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    Serial.println("\t\tCell Temperatures\t\t\t\t\t\t\t\t\t   PCB Temperatures");
+    Serial.println("\tTHERM 0\t\tTHERM 1\t\tTHERM 2\t\t\t\t\t\t\t\tTHERM 0\t\tTHERM 1");
+    for (int ic = 0; ic < TOTAL_IC; ic++) {
+        Serial.print("IC"); Serial.print(ic); Serial.print("\t");
+        for (int therm = 0; therm < THERMISTORS_PER_IC; therm++) {
+            double temp = ((double) bms_detailed_temperatures[ic].get_temperature(therm)) / 100;
+            Serial.print(temp, 2);
+            Serial.print(" ºC");
+            Serial.print("\t");
+        }
+        Serial.print("\t\t\t\t\t\t");
+        for (int therm = 0; therm < PCB_THERM_PER_IC; therm++) {
+            double temp = ((double) bms_onboard_detailed_temperatures[ic].get_temperature(therm)) / 100;
+            Serial.print(temp, 2);
+            Serial.print(" ºC");
+            Serial.print("\t");
+        }
+        Serial.println();
+    }
+    Serial.print("\nCell temperature statistics\t\t Average: ");
+    Serial.print(bms_temperatures.get_average_temperature() / (double) 100, 2);
+    Serial.print(" ºC\t\t");
+    Serial.print("Min: ");
+    Serial.print(bms_temperatures.get_low_temperature() / (double) 100, 2);
+    Serial.print(" ºC\t\t");
+    Serial.print("Max: ");
+    Serial.print(bms_temperatures.get_high_temperature() / (double) 100, 2);
+    Serial.println(" ºC\n");
+    Serial.print("PCB temperature statistics\t\t Average: ");
+    Serial.print(bms_onboard_temperatures.get_average_temperature() / (double) 100, 2);
+    Serial.print(" ºC\t\t");
+    Serial.print("Min: ");
+    Serial.print(bms_onboard_temperatures.get_low_temperature() / (double) 100, 2);
+    Serial.print(" ºC\t\t");
+    Serial.print("Max: ");
+    Serial.print(bms_onboard_temperatures.get_high_temperature() / (double) 100, 2);
+    Serial.println(" ºC\n");
+}
+
+#endif
