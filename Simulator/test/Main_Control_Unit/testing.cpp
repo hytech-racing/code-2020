1422db19a0b1d3ef677b30073c458fb11e8a57c3
BZhu792 <bohanzhu9@gmail.com>
2021-06-03 18:29:05 -0400

    Uploaded faulty mcu testcode to find resolution
    MCU's testing.cpp runs tests from gtest_main.cc (0 tests) instead of running the mcu_test gtest. Dashboard testing.cpp works.

--- Simulator/test/Main_Control_Unit/testing.cpp
+++ Simulator/test/Main_Control_Unit/testing.cpp
@@ -15,9 +15,21 @@ protected:
 	MCUTesting () {}
 	void SetUp() {
         // declare as extern all variables in mcu code that need to be reset for each new test
+		extern bool is_mc_err;
+		extern VariableLED variable_led_start;
+		extern Metro timer_can_update;
+		extern MCP_CAN CAN;
+		extern Dashboard_status dashboard_status;
+		extern MCU_status mcu_status;
 
 		MockCAN::teardown();
         //clear those variables
+		is_mc_err = false;
+		variable_led_start = VariableLED(LED_START);
+		timer_can_update = Metro(100);
+		CAN = MCP_CAN(SPI_CS);
+		dashboard_status = {};
+		mcu_status = {};
 		
         
 		Simulator::begin();
@@ -27,3 +39,22 @@ protected:
 		Simulator::teardown();
 	}
 };
+
+TEST_F(MCUTesting, STARTUP){
+	CAN_message_t msg;
+	delay(100);
+	next();
+	ASSERT_TRUE(MockCAN::read(msg));
+	ASSERT_EQ(msg.id,ID_MCU_STATUS);
+	/*
+	MCU_status status;
+	status.load(msg.buf);
+	EXPECT_EQ(status.get_button_flags(),0);
+	EXPECT_EQ(status.get_led_flags(),3);
+	*/
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
